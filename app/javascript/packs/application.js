0c63f55a80b1d524a7ad6c07299743989216a1bb
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-02-08 02:00:37 +0100

    bcp de choses

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,46 +3,54 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
-import "bootstrap";
+// require("@rails/ujs").start()
+// require("@rails/activestorage").start()
+// require("channels")
 
 const faders = document.querySelectorAll('.fade-in');
 const sliders = document.querySelectorAll('.slide-in');
-const openHome = document.querySelector('.open-home');
+const openHomes = document.querySelectorAll('.btn-open');
 const closeHome = document.querySelector('.close-home');
-const anchorPhoto = document.querySelector('.anchor-photo');
+// const anchors = document.querySelectorAll('.anchor');
+// const cursor = document.querySelector('.cursor');
 
+openHomes.forEach(openHome => {
+  openHome.addEventListener("click", function () {
 
-openHome.addEventListener("click", function () {
-  sliders.forEach(slider => {
-    slider.classList.add('appear')
+    sliders.forEach(slider => {
+      slider.classList.add('appear')
+    });
+
+    faders.forEach(fader => {
+      fader.classList.add('appear')
+    });
+
+    closeHome.classList.add('appear');
+
+    openHomes.forEach(openHome => {
+      openHome.classList.add('hidden')
+    });
   })
-  faders.forEach(fader => {
-    fader.classList.add('appear')
-  })
-  openHome.classList.add('hidden')
 })
 
 closeHome.addEventListener("click", function () {
-  sliders.forEach(slider => {
-    slider.classList.remove('appear')
-  })
-  faders.forEach(fader => {
-    fader.classList.remove('appear')
-  })
-  openHome.classList.remove('hidden')
-})
 
-anchorPhoto.addEventListener("click", function () {
   sliders.forEach(slider => {
     slider.classList.remove('appear')
-  })
+  });
+
   faders.forEach(fader => {
     fader.classList.remove('appear')
-  })
-  openHome.classList.remove('hidden')
-})
+  });
+
+  closeHome.classList.remove('appear');
+
+  openHomes.forEach(openHome => {
+    openHome.classList.remove('hidden')
+  });
+});
+
+// document.addEventListener('mousemove', (e) => {
+//   cursor.style.left = e.pageX + 'px'
+//   cursor.style.top = e.pageY + 'px'
+// })

e3e4b8ee6acad146c09289633f5aa044359fccb7
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-03-23 00:02:07 +0100

    db fix

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_12_11_150949) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "photos", force: :cascade do |t|
+  create_table "sites", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
